0a1e409b620f90542a77e028aafafbbf6e18157c
ellysaschottler <ellysastork@gmail.com>
2024-04-02 11:41:22 -0500

    add logs functioning

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -34,7 +34,31 @@ export const getLogs = () => async dispatch => {
     }
 }
 
+// Add new log
+export const addLog = (log) => async dispatch => {
+    try {
+        setLoading();
 
+        const res = await fetch('/logs', {
+            method: 'POST', 
+            body: JSON.stringify(log), 
+            headers: {
+                'Content-Type': "application/json"
+            }
+    });
+        const data = await res.json();
+
+        dispatch({
+            type: ADD_LOG,
+            payload: data
+        })
+    } catch (err){
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+}
 
 // SET LOADING TO TRUE
 export const setLoading = () => {
