edc322972788cbc5f665861319ec429c5fd528bf
ellysaschottler <ellysastork@gmail.com>
2024-04-08 11:08:30 -0500

    delete tech function completed

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -53,3 +53,23 @@ export const setLoading = () => {
        type: SET_LOADING
     }   
    }
+
+   
+// Delete tech from server
+export const deleteTech = (id) => async dispatch => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, { method: 'DELETE' });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText
+    });
+  }
+};
